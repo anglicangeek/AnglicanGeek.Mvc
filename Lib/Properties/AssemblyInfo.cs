62c41cd132066a47e91d0e93f5f1aa1c5f7d60ae
Drew Miller (anglicangeek) <ego@anglicangeek.com>
2011-10-01 11:07:13 -0700

    updated project metadata to remove my email address and increase the version

--- Lib/Properties/AssemblyInfo.cs
+++ Lib/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("AnglicanGeek.Mvc")]
 [assembly: AssemblyProduct("AnglicanGeek.Mvc")]
-[assembly: AssemblyCompany("Andrew Miller <ego@anglicangeek.com>")]
-[assembly: AssemblyCopyright("Copyright (C) 2011 Andrew Miller <ego@anglicangeek.com>")]
-[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyCompany("Drew Miller (anglicangeek)")]
+[assembly: AssemblyCopyright("Copyright (C) 2011 Andrew Miller")]
+[assembly: AssemblyVersion("0.3.0.0")]
 [assembly: ComVisible(false)]
