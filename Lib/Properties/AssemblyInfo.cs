f10570ef852cb61e458b857fa363bca46a4bf69f
Drew Miller (anglicangeek) <ego@anglicangeek.com>
2011-10-01 13:18:43 -0700

    changed the .nuspec to use tokens and changed assembly metadata accordingly

--- Lib/Properties/AssemblyInfo.cs
+++ Lib/Properties/AssemblyInfo.cs
@@ -1,8 +1,9 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("AnglicanGeek.Mvc")]
 [assembly: AssemblyProduct("AnglicanGeek.Mvc")]
+[assembly: AssemblyTitle("AnglicanGeek.Mvc")]
+[assembly: AssemblyDescription("A library that has the stuff I use with *every* ASP.NET MVC app I craft.")]
 [assembly: AssemblyCompany("Drew Miller (anglicangeek)")]
 [assembly: AssemblyCopyright("Copyright (C) 2011 Andrew Miller")]
 [assembly: AssemblyVersion("0.3.0.0")]
